96259e88eb68cceca4f3b45a51b351f597bd6812
FreezeBerries <michaelhunt.is@gmail.com>
2020-03-30 18:31:57 -0500

    Video 30 comment removed

--- reactjs/hello-world/src/components/FRInput.js
+++ reactjs/hello-world/src/components/FRInput.js
@@ -1,12 +1,5 @@
 import React from 'react'
 
-// function FRInput() {
-//     return (
-//         <div>
-//             <input type="text" />
-//         </div>
-//     )
-// }
 const FRInput = React.forwardRef((props, ref) => {
     return (
         <div>

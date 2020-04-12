2fc3438c3ae2ecbd7217fe84e25412648e29d583
FreezeBerries <michaelhunt.is@gmail.com>
2020-04-11 20:05:22 -0500

    video 47 completed

--- reactjs/state-hook/src/App.js
+++ reactjs/state-hook/src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter from './components/HookCounter';
+import HookCounter3 from './components/HookCounter3';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
-      <HookCounter2 />
+      <HookCounter3 />
     </div>
   );
 }

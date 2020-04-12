97aaa4094b420bd96f4937d9a812b747cf1935c0
FreezeBerries <michaelhunt.is@gmail.com>
2020-04-12 17:21:33 -0500

    video 56 completed

--- reactjs/readme.md
+++ reactjs/readme.md
@@ -1874,3 +1874,41 @@ function DataFetching() {
 
 export default DataFetching;
 ```
+
+#### 56th video
+
+This video is about request only one post
+
+```jsx
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function DataFetching() {
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  return (
+    <div>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {/* posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))*/}
+    </div>
+  );
+}
+
+export default DataFetching;
+```
--- reactjs/state-hook/src/components/DataFetching.js
+++ reactjs/state-hook/src/components/DataFetching.js
@@ -1,26 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function DataFetching() {
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({});
+    const [id, setId] = useState(1);
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                console.log(res)
-                setPosts(res.data);
-            }).catch(err => {
-                console.log(err)
+        axios
+            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then((res) => {
+                console.log(res);
+                setPost(res.data);
             })
-    }, [])
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [id]);
 
     return (
         <div>
-            {
-                posts.map(post => <li key={post.id}>{post.title}</li>)
-            }
+            <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+            <div>{post.title}</div>
+            {/* posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))*/}
         </div>
-    )
+    );
 }
 
 export default DataFetching

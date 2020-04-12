2b3bc610ea2c23722ac60fe9cdd7ddf154c62068
FreezeBerries <michaelhunt.is@gmail.com>
2020-04-11 19:40:03 -0500

    video 45 completed

---
+++ reactjs/state-hook/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      <HookCounter />
+    </div>
+  );
+}
+
+export default App;

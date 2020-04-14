a5bb7c9198bdb4b03e88cd540c01aee3b09a57ec
FreezeBerries <michaelhunt.is@gmail.com>
2020-04-13 19:07:26 -0500

    video 66 completed

---
+++ reactjs/reducer-hook/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DataFetchingOne from './component/DataFetchingOne';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetchingOne />
+    </div>
+  );
+}
+
+export default App;
